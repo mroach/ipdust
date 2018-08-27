3021455e98fe1154f25c7ed69afabd4ceaf63c79
mroach <mroach@mroach.com>
2018-08-27 20:06:46 +0200

    Package upgrades

--- mix.exs
+++ mix.exs
@@ -38,17 +38,16 @@ defmodule Ipdust.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.3"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.3.4"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.10"},
+      {:phoenix_html, "~> 2.12"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:timex, "~> 3.1"},
-      {:edeliver, "~> 1.5"},
-      {:distillery, "~> 1.5", runtime: false},
+      {:edeliver, "~> 1.6"},
+      {:distillery, "~> 2.0", runtime: false, warn_missing: false},
       {:geolix, "~> 0.16"},
       {:remote_ip, "~> 0.1.0"},
       {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
