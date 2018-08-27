e839446e88257c701438d668c412709d616eb2d3
mroach <mroach@mroach.com>
2018-08-27 20:07:00 +0200

    Fix warning

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Ipdust.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 
